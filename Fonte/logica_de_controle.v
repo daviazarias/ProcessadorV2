33e2ad6027d19af09e2aa7d4c64bccd99cf909bd
Davi Azarias <davi.cabral@sou.unifal-mg.edu.br>
2025-06-06 23:52:18 -0300

    Terceiro commit

--- Fonte/logica_de_controle.v
+++ Fonte/logica_de_controle.v
@@ -1,63 +1,44 @@
 `include "decodificador.v"
 
 module logica_de_controle(
-    input            resetn,
-    input      [1:0] counter,
-    input      [8:0] iin,
-    output reg [3:0] mux_select,
-    output     [7:0] regs_enable,
-    output     [1:0] alu_op_select,
-    output           reg_a_enable,
-    output           alu_output_enable,
-    output           clear
+    input        resetn,
+    input  [1:0] counter,
+    input  [8:0] iin,
+    output [2:0] reg_address,
+    output [7:0] regs_enable,
+    output [1:0] alu_op_select,
+    output       reg_a_enable,
+    output       imm_select,
+    output       alu_output_enable,
+    output       alu_output_select,
+    output       clear
 );
 
 `define REP 3'b111
 `define LDI 3'b101
-`define ADD 3'b000
-`define SUB 3'b001
-`define NAN 3'b010
 `define OUT 3'b100
 
-`define IMM_SELECT 4'b1000 
-`define R_SELECT   4'b1001
-
-reg [2:0] opcode, rx, ry;
-
+reg  [2:0] opcode, rx, ry;
 wire [7:0] regs_select;
 decodificador decode(rx, regs_select);
 
 assign reg_a_enable      = (counter == 2'b01); 
+assign reg_address       = (counter == 2'b01) ? {1'b0, rx} : {1'b0, ry};
 assign alu_output_enable = (counter == 2'b10);
+assign alu_output_select = (counter == 2'b11 && opcode != `REP && opcode != `LDI);
+assign imm_select        = (counter == 2'b11 && opcode == `LDI);
 assign regs_enable       = (counter == 2'b11 && opcode != `OUT) ? regs_select : 8'h00;
+
 assign alu_op_select     = (!opcode[2]) ? opcode[1:0] : 2'b11;
 assign clear             = resetn;
 
-always @(counter, resetn)
+always @(counter) 
 begin
-    case(counter)
-
-        2'b00: begin
-            opcode <= iin[8:6];
-            rx     <= iin[5:3];
-            ry     <= iin[2:0];
-        end
-
-        2'b01: mux_select <= {1'b0, rx};
-
-        2'b10: mux_select <= {1'b0, ry};
-
-        2'b11: begin
-
-            case(opcode)
-                `REP:    mux_select <= {1'b0, ry};
-                `LDI:    mux_select <= `IMM_SELECT;
-                default: mux_select <= `R_SELECT;
-            endcase
-
-        end
-
-    endcase
+    if(counter == 2'b00) begin
+        opcode <= iin[8:6];
+        rx     <= iin[5:3];
+        ry     <= iin[2:0];
+    end
 end
 
 endmodule
--- Fonte/processador.v
+++ Fonte/processador.v
@@ -10,30 +10,30 @@ module processador(
     output [15:0] bus
 );
 
-wire [3:0] mux_select;
+wire [2:0] reg_address;
 wire [7:0] regs_enable;
 wire [1:0] alu_op_select;
 wire [1:0] saida_contador;
-wire [15:0] mux_out, imm;
+wire [15:0] data_bus, imm, reg_a_out, alu_out;
 wire [15:0] r0_out, r1_out, r2_out, r3_out, r4_out, r5_out, r6_out, r7_out;
-wire [15:0] reg_a_out, alu_out;
 
-contador cont(clock,clear,saida_contador);
-extensor ext(iin[9:0],imm);
-logica_de_controle log(resetn,saida_contador,iin[15:7],
-    mux_select,regs_enable,alu_op_select,reg_a_enable,alu_output_enable,clear);
+contador cont(clock, clear, saida_contador);
+extensor ext(iin[9:0], imm);
+logica_de_controle log(resetn, saida_contador, iin[15:7],
+    reg_address, regs_enable, alu_op_select, reg_a_enable, imm_select,
+    alu_output_enable, alu_output_select, clear);
 
-registrador r0(regs_enable[0],clock,mux_out,r0_out);
-registrador r1(regs_enable[1],clock,mux_out,r1_out);
-registrador r2(regs_enable[2],clock,mux_out,r2_out);
-registrador r3(regs_enable[3],clock,mux_out,r3_out);
-registrador r4(regs_enable[4],clock,mux_out,r4_out);
-registrador r5(regs_enable[5],clock,mux_out,r5_out);
-registrador r6(regs_enable[6],clock,mux_out,r6_out);
-registrador r7(regs_enable[7],clock,mux_out,r7_out);
-registrador a(reg_a_enable,clock,mux_out,reg_a_out);
-registrador r(alu_output_enable,clock,mux_out,alu_out);
+registrador r0(regs_enable[0],clock,data_bus,r0_out);
+registrador r1(regs_enable[1],clock,data_bus,r1_out);
+registrador r2(regs_enable[2],clock,data_bus,r2_out);
+registrador r3(regs_enable[3],clock,data_bus,r3_out);
+registrador r4(regs_enable[4],clock,data_bus,r4_out);
+registrador r5(regs_enable[5],clock,data_bus,r5_out);
+registrador r6(regs_enable[6],clock,data_bus,r6_out);
+registrador r7(regs_enable[7],clock,data_bus,r7_out);
+registrador a(reg_a_enable,clock,data_bus,reg_a_out);
+registrador r(alu_output_enable,clock,data_bus,alu_out);
 
-assign bus = mux_out;
+assign bus = data_bus;
 
 endmodule;
