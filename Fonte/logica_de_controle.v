43e672ec295f09daeafdb86f82286f63788b43a0
Davi Azarias <davi.cabral@sou.unifal-mg.edu.br>
2025-07-11 12:35:51 -0300

    Apenas organizando

--- Fonte/logica_de_controle.v
+++ Fonte/logica_de_controle.v
@@ -24,38 +24,46 @@ module logica_de_controle(
 `define BEZ 3'b110
 `define HLT 3'b011
 
-`define ALU_OPERATION (opcode == `ADD || opcode == `SUB || opcode == `NAN)
-`define RX_SELECT      {1'b0,rx}
-`define RY_SELECT      {1'b0,ry}
-`define IMM_SELECT      4'b1000 
-`define ALU_OUT_SELECT  4'b1001 
-`define NO_OUTPUT       4'b1111
-`define NO_OP            2'b11
+`define ALU_OPERATION      (opcode == `ADD || opcode == `SUB || opcode == `NAN)
+`define NO_WRITE_OPERATION (opcode != `OUT && opcode != `BEZ)
+`define RX_SELECT          {1'b0,rx}
+`define RY_SELECT          {1'b0,ry}
+`define IMM_SELECT         4'b1000 
+`define ALU_OUT_SELECT     4'b1001 
+`define NO_OUTPUT          4'b1111
+`define NO_OP              2'b11
 
 reg  [2:0] opcode, rx, ry;
 wire [7:0] regs_select;
 decodificador dec(rx, regs_select);
 
 // Habilita a escrita no registrador de entrada da ULA.
+
 assign a_reg_enable   = (counter == 2'b01 && `ALU_OPERATION); 
 
 // Habilita a escrita no regitrador de saída da ULA.
+
 assign alu_reg_enable = (counter == 2'b10 && `ALU_OPERATION);
 
 // Determina qual operação deve ser realizada pela ULA.
+
 assign alu_op_select  = (`ALU_OPERATION) ? opcode[1:0] : `NO_OP;
 
 // Cada elemento desse array de 8 bits é ligado ao campo 
 // 'wr_enable' de um dos 8 registradores endereçáveis.
-assign regs_enable    = (counter == 2'b11 && opcode != `OUT) ? regs_select : 8'h00;
+
+assign regs_enable    = (counter == 2'b11 && `NO_WRITE_OPERATION) ? regs_select : 8'h00;
 
 // Habilita a escrita no registrador de imediato.
+
 assign imm_wr_enable  = (counter == 2'b00);
 
 // Habilita a escrita no contador de programa.
+
 assign pc_wr_enable   = (counter == 2'b11);
 
 // Indica que a operação atual é um desvio condicional.
+
 assign branch_select  = (opcode == `BEZ);
 
 always @(counter) 
@@ -88,7 +96,6 @@ begin
                 `LDI      : mux_select <= `IMM_SELECT;
                 default   : mux_select <= `ALU_OUT_SELECT;
             endcase
-
     endcase
 end
 
--- testbench.v
+++ testbench.v
@@ -17,12 +17,19 @@ initial $dumpvars(0,testbench);
 computador c(clock, resetn, mem_wr, iin, endereco_ext, bus);
 
 initial begin
-    # 0 iin = 16'b101_000_1111111101;
-    # 2 iin = 16'b101_001_0000000001;
-    # 2 iin = 16'b010_000_000_0000000;
-    # 2 iin = 16'b000_000_001_0000000;
-    # 2 iin = 16'b100_000_000_0000000;
-    # 2 iin = 16'b011_000_0000000000;
+    # 0 iin = 16'b101_000_0000000000;  // ldi r0 #0
+    # 2 iin = 16'b101_001_0000000001;  // ldi r1 #1
+    # 2 iin = 16'b101_010_0000000101;  // ldi r2 #5
+    # 2 iin = 16'b101_011_0000000011;  // ldi r3 #3
+    # 2 iin = 16'b101_100_0000000000;  // ldi r4 #0
+
+    # 2 iin = 16'b110_011_0000000100;  // bez r3 #4
+    # 2 iin = 16'b000_100_010_xxxxxxx; // add r4, r2
+    # 2 iin = 16'b001_011_001_xxxxxxx; // sub r3 r1
+    # 2 iin = 16'b110_000_1111111101;  // bez r0 #-3
+
+    # 2 iin = 16'b100_100_xxxxxxxxxx;  // out r4
+    # 2 iin = 16'b011_xxx_xxxxxxxxxx;  // hlt
 
     # 2 mem_wr <= 1'b0;
     # 1 resetn <= 1'b0;
